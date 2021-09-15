d36f713bad05f59ee23100cd7979966bf3fa93b2
Josh1950 <90259914+Josh1950@users.noreply.github.com>
2021-09-15 09:00:50 -0400

    Add files via upload

---
+++ TechAssignment2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Conditional_Statement
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Expected Grade...");
+
+            string input = Console.ReadLine();
+
+
+            decimal grade = decimal.Parse(input);
+
+
+
+            if (grade >= 90 && grade <= 100)
+            {
+                Console.WriteLine("You receive an A");
+
+            }
+
+            else if (grade >= 80 && grade <= 90)
+            {
+                Console.WriteLine("You receive a B");
+            }
+            else if (grade >= 70 && grade <= 80)
+            {
+                Console.WriteLine("You receive a C");
+            }
+            else if (grade >= 60 && grade <= 70)
+            {
+                Console.WriteLine("You receive a D");
+            }
+
+			else if (grade <= 70)
+            {
+                Console.WriteLine("You receive an F");
+            }
+        }
+    }
+}
